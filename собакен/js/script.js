005de8ed213f4fab8db87aa874ffd7a8a2a663af
Akai <59208058+SuuPS@users.noreply.github.com>
2020-07-12 13:39:50 +0600

    Add files via upload

---
+++ собакен/js/script.js
@@ -0,0 +1,61 @@
+
+
+
+let modal  = document.querySelector('.modal');
+let modal1 = document.querySelector('.modal-1');
+let modal2 = document.querySelector('.modal-2');
+let modal3 = document.querySelector('.modal-3');
+let modal4 = document.querySelector('.modal-4');
+let modal5 = document.querySelector('.modal-5');
+let modal6 = document.querySelector('.modal-6');
+let modal7 = document.querySelector('.modal-7');
+let modal8 = document.querySelector('.modal-8');
+open1.onclick = function (argument) {
+	modal1.style.display = 'flex';
+}
+open2.onclick = function (argument) {
+	modal2.style.display = 'flex';
+}
+open3.onclick = function (argument) {
+	modal3.style.display = 'flex';
+}
+open4.onclick = function (argument) {
+	modal4.style.display = 'flex';
+}
+open5.onclick = function (argument) {
+	modal5.style.display = 'flex';
+}
+open6.onclick = function (argument) {
+	modal6.style.display = 'flex';
+}
+open7.onclick = function (argument) {
+	modal7.style.display = 'flex';
+}
+open8.onclick = function (argument) {
+	modal8.style.display = 'flex';
+}
+
+closeModal.onclick = function (argument) {
+	modal1.style.display = ' none';
+}
+closeModal2.onclick = function (argument) {
+	modal2.style.display = ' none';
+}
+closeModal3.onclick = function (argument) {
+	modal3.style.display = ' none';
+}
+closeModal4.onclick = function (argument) {
+	modal4.style.display = ' none';
+}
+closeModal5.onclick = function (argument) {
+	modal5.style.display = ' none';
+}
+closeModal6.onclick = function (argument) {
+	modal6.style.display = ' none';
+}
+closeModal7.onclick = function (argument) {
+	modal7.style.display = ' none';
+}
+closeModal8.onclick = function (argument) {
+	modal8.style.display = ' none';
+}
