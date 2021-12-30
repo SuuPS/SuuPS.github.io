e994e60b51d432909a6b104ab29c85ab8ddc1eda
Akai <59208058+SuuPS@users.noreply.github.com>
2021-12-30 13:55:21 +0600

    Delete ar-rr directory

--- ar-rr/server.py
+++
@@ -1,6 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import ssl
-
-httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='mycertificate.crt', keyfile="mycertificate.crt", server_side=True)
-httpd.serve_forever()
